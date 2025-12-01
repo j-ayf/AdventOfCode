9f54983c56e59ae4bf133be77afa613a49737b45
Janik Frost <janik.frost@tui.com>
2025-12-01 13:51:22 +0100

    2025 Day 1 Part 2 for sample only

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -9,9 +9,10 @@ def main():
     dial = list(range(100))
     current_position = 50
 
-    print(f'Zeros: {count_zeroes(puzzle_input, dial, current_position)}')
+    print(f'Zeros: {part1(puzzle_input, dial, current_position)}')
+    print(f'Zeros: {part2(puzzle_input, dial, current_position)}')
 
-def count_zeroes(puzzle_input, dial, current_position):
+def part1(puzzle_input, dial, current_position):
     zero_counter = 0
     for turn in puzzle_input:
         current_position += get_direction(turn)
@@ -20,9 +21,37 @@ def count_zeroes(puzzle_input, dial, current_position):
         while current_position < 0:
             current_position += len(dial)
 
-        print(current_position, get_direction(turn))
+        # print(current_position, get_direction(turn))
         if dial[current_position] == 0:
             zero_counter += 1
+
+    return zero_counter
+
+
+def part2(puzzle_input, dial, current_position, was_zero=False):
+    zero_counter = 0
+    for turn in puzzle_input:
+        current_position += get_direction(turn)
+        while current_position > len(dial):
+            if not was_zero:
+                zero_counter += 1
+            else:
+                was_zero = False
+            current_position -= len(dial)
+        while current_position < 0:
+            if not was_zero:
+                zero_counter += 1
+            else:
+                was_zero = False
+            current_position += len(dial)
+
+        # print(current_position, get_direction(turn))
+        if current_position in [0, len(dial)]:
+            zero_counter += 1
+            current_position = 0
+            was_zero = True
+        else:
+            was_zero = False
 
     return zero_counter
 

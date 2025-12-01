043d274959cb497335c2123ca3b14ba3f1670c90
Janik Frost <janik.frost@tui.com>
2025-12-01 15:50:52 +0100

    Add folder structure for every day

---
+++ 2025/12/main.py
@@ -0,0 +1,24 @@
+# https://adventofcode.com/2025/day/12
+
+def get_input():
+    with open('puzzleInput.txt', 'r') as f:
+        return f.read().rstrip().split('\n')
+
+
+def main():
+    puzzle_input = get_input()
+
+    print(f'Part1: {part1()}')
+    print(f'Part2: {part2()}')
+
+
+def part1():
+    return 0
+
+
+def part2():
+    return 0
+
+
+if __name__ == '__main__':
+    main()

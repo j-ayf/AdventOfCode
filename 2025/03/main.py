2e7e1595b99766253f936a78df5807a787e90fb8
Janik Frost <janik.frost@tui.com>
2025-12-03 16:28:55 +0100

    2025 Day 3 Part 1 - Use itertools

--- 2025/03/main.py
+++ 2025/03/main.py
@@ -1,4 +1,6 @@
 # https://adventofcode.com/2025/day/3
+import itertools
+
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
@@ -15,28 +17,13 @@ def main():
 def part1(puzzle_input):
     max_joltage = 0
     for bank in puzzle_input:
-        reversed_bank = list(bank).copy()
-        reversed_bank.reverse()
-        index_1 = 0
-        index_2 = 0
-        max_num = 0
-        for i in range(len(reversed_bank)):
-            if i == 0: continue
-            current_num = int(reversed_bank[i])
-            if current_num >= max_num:
-                max_num = current_num
-                index_1 = i
-        max_num = 0
-        i = len(bank)-index_1
-        while i < len(bank):
-            current_num = int(bank[i])
-            if current_num > max_num:
-                max_num = current_num
-                index_2 = i
-            i += 1
-
-        max_joltage += int(reversed_bank[index_1] + bank[index_2])
-
+        t = itertools.combinations(bank, 2)
+        l = list(t)
+        l.sort(reverse=True)
+        num_str = ''
+        for num in l[0]:
+            num_str += num
+        max_joltage += int(num_str)
     return max_joltage
 
 

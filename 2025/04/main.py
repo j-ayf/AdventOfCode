da7fcebe8ded65e21b64275f6e80d2b104b98a6f
Janik Frost <janik.frost@tui.com>
2025-12-04 15:27:15 +0100

    2025 Day 4 Part 2

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -2,28 +2,60 @@
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
-        return f.read().rstrip().split('\n')
+        puzzle = f.read().rstrip().split('\n')
+    full_list = []
+    for row in puzzle:
+        full_list.append(list(row))
+    return full_list
 
 
 def main():
     puzzle_input = get_input()
 
     print(f'Part1: {part1(puzzle_input)}')
-    print(f'Part2: {part2()}')
+    print(f'Part2: {part2(puzzle_input)}')
 
 
 def part1(puzzle_input):
+    return check_for_rolls(puzzle_input)[0]
+
+
+def part2(puzzle_input):
     sum_rolls = 0
+    puzzle_copy = puzzle_input.copy()
+
+    while True:
+        check = check_for_rolls(puzzle_copy)
+        removed_rolls = len(check[1])
+        if removed_rolls == 0:
+            break
+
+        sum_rolls += removed_rolls
+        puzzle_copy = remove_rolls(puzzle_copy, check[1])
+
+    return sum_rolls
+
+
+def remove_rolls(puzzle_input: list, coords):
+    puzzle_copy = puzzle_input.copy()
+    for coord in coords:
+        puzzle_copy[coord[0]][coord[1]] = '.'
+    return puzzle_copy
+
+
+def check_for_rolls(puzzle_input):
+    sum_rolls = 0
+    removed_coords = []
     for i in range(len(puzzle_input)):
-        if i-1 < 0:
+        if i - 1 < 0:
             above = None
         else:
-            above = puzzle_input[i-1]
+            above = puzzle_input[i - 1]
 
-        if i+1 >= len(puzzle_input):
+        if i + 1 >= len(puzzle_input):
             below = None
         else:
-            below = puzzle_input[i+1]
+            below = puzzle_input[i + 1]
 
         for j in range(len(puzzle_input[i])):
             if puzzle_input[i][j] == '.':
@@ -34,12 +66,9 @@ def part1(puzzle_input):
                 continue
             else:
                 sum_rolls += 1
+                removed_coords.append([i, j])
 
-    return sum_rolls
-
-
-def part2():
-    return 0
+    return [sum_rolls, removed_coords]
 
 
 def check_row(row, above, below, roll_index, row_length):

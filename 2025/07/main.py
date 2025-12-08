e7e376fb19d73f37cf474b6c18c3082ce29e01a9
Janik Frost <janik.frost@tui.com>
2025-12-08 15:13:04 +0100

    2025 Day 7 Part 1

--- 2025/07/main.py
+++ 2025/07/main.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2025/day/7
+import re
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
@@ -8,17 +9,42 @@ def get_input():
 def main():
     puzzle_input = get_input()
 
-    print(f'Part1: {part1()}')
+    print(f'Part1: {part1(puzzle_input)}')
     print(f'Part2: {part2()}')
 
 
-def part1():
-    return 0
+def part1(puzzle):
+    total_splits = 0
+    beam_indexes = []
+    for i in range(len(puzzle)):
+        if i == 0:
+            beam_indexes.append(puzzle[i].find('S'))
+            continue
+        splitter_indices = get_splitter_indices(puzzle[i])
+        if splitter_indices:
+            for splitter_i in splitter_indices:
+                if splitter_i in beam_indexes:
+                    beam_indexes.remove(splitter_i)
+                    total_splits += 1
+                    if splitter_i-1 not in beam_indexes:
+                        beam_indexes.append(splitter_i-1)
+                        beam_indexes.sort()
+                    if splitter_i+1 not in beam_indexes:
+                        beam_indexes.append(splitter_i+1)
+                        beam_indexes.sort()
+
+    return total_splits
 
 
 def part2():
     return 0
 
+
+def get_splitter_indices(line):
+    indices = []
+    for splitter in re.finditer(r'\^', line):
+        indices.append(splitter.start())
+    return indices
 
 if __name__ == '__main__':
     main()

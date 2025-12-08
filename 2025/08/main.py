582981c22ddf8e21c1d90e1a2c5c731906c7050c
Janik Frost <janik.frost@tui.com>
2025-12-08 17:10:58 +0100

    2025 Day 8 Part 1 - Add initial calculation

--- 2025/08/main.py
+++ 2025/08/main.py
@@ -1,4 +1,15 @@
 # https://adventofcode.com/2025/day/8
+import math
+
+
+class JunctionBox:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f'X:{self.x},Y:{self.y},Z:{self.z}'
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
@@ -12,13 +23,23 @@ def get_input():
 
 def main():
     puzzle_input = get_input()
-    print(puzzle_input)
+    print(puzzle_input)  # todo
+    print(get_distance(puzzle_input[0], puzzle_input[0]))  # todo
 
-    print(f'Part1: {part1()}')
+    print(f'Part1: {part1(puzzle_input)}')
     print(f'Part2: {part2()}')
 
 
-def part1():
+def part1(boxes):
+    for box in boxes:
+        lowest_distance = 9999999999999999999999999999999999999999999.0
+        for box2 in boxes:
+            distance = get_distance(box, box2)
+            if distance == 0:
+                continue
+            elif distance < lowest_distance:
+                lowest_distance = distance
+        print(lowest_distance)  # todo
     return 0
 
 
@@ -26,14 +47,9 @@ def part2():
     return 0
 
 
-class JunctionBox:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __repr__(self):
-        return f'X:{self.x},Y:{self.y},Z:{self.z}'
+def get_distance(box1, box2):
+    """https://en.wikipedia.org/wiki/Euclidean_distance"""
+    return math.sqrt((box1.x - box2.x)**2 + (box1.y - box2.y)**2 + (box1.z - box2.z)**2)
 
 
 if __name__ == '__main__':

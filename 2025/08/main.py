71c87a82dd338a0ae7eadede453ce8a9904ebc83
Janik Frost <janik.frost@tui.com>
2025-12-08 16:54:23 +0100

    2025 Day 8 Part 1 - Parse coordinates

--- 2025/08/main.py
+++ 2025/08/main.py
@@ -2,11 +2,17 @@
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
-        return f.read().rstrip().split('\n')
+        lines = f.read().rstrip().split('\n')
+        boxes = []
+        for line in lines:
+            coords = line.split(',')
+            boxes.append(JunctionBox(int(coords[0]), int(coords[1]), int(coords[2])))
+        return boxes
 
 
 def main():
     puzzle_input = get_input()
+    print(puzzle_input)
 
     print(f'Part1: {part1()}')
     print(f'Part2: {part2()}')
@@ -18,6 +24,16 @@ def part1():
 
 def part2():
     return 0
+
+
+class JunctionBox:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f'X:{self.x},Y:{self.y},Z:{self.z}'
 
 
 if __name__ == '__main__':

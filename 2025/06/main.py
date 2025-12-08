ddd355ace260d0f1a4e109fe6be89452711ae472
Janik Frost <janik.frost@tui.com>
2025-12-08 11:51:37 +0100

    2025 Day 6 Part 1

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -1,23 +1,51 @@
 # https://adventofcode.com/2025/day/6
+import math
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
         return f.read().rstrip().split('\n')
 
 
-def main():
-    puzzle_input = get_input()
+def split_part1(puzzle_input):
+    lines = []
+    for line in puzzle_input:
+        lines.append(line.split())
+    return lines
 
+
+def main():
     print(f'Part1: {part1()}')
     print(f'Part2: {part2()}')
 
 
 def part1():
-    return 0
+    puzzle = split_part1(get_input())
+    total = 0
+    for i in range(len(puzzle[0])):
+        nums = get_nums(puzzle, i)
+        total += solve_problem(nums, puzzle[-1][i])
+
+    return total
 
 
 def part2():
     return 0
+
+
+def get_nums(puzzle, index):
+    nums = []
+    for i in range(len(puzzle)-1):
+        nums.append(int(puzzle[i][index]))
+    return nums
+
+
+def solve_problem(nums, operator):
+    if operator == '+':
+        return sum(nums)
+    elif operator == '*':
+        return math.prod(nums)
+    else:
+        raise Exception(f'Unknown operator {operator}')
 
 
 if __name__ == '__main__':

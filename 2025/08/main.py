f499e4f56c2058b49650b6c5cc4a4f2bfa0da88d
Janik Frost <janik.frost@tui.com>
2025-12-09 12:45:52 +0100

    2025 Day 8 Part 1 - Add additional logic. Loop runs one too many times, because one pair is already in the same network

--- 2025/08/main.py
+++ 2025/08/main.py
@@ -11,6 +11,15 @@ class JunctionBox:
     def __repr__(self):
         return f'X:{self.x},Y:{self.y},Z:{self.z}'
 
+
+class Network:
+    def __init__(self, box1, box2):
+        self.boxes = [box1, box2]
+
+    def __repr__(self):
+        return f'{self.boxes}'
+
+
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
         lines = f.read().rstrip().split('\n')
@@ -24,22 +33,42 @@ def get_input():
 def main():
     puzzle_input = get_input()
     print(puzzle_input)  # todo
-    print(get_distance(puzzle_input[0], puzzle_input[0]))  # todo
 
     print(f'Part1: {part1(puzzle_input)}')
     print(f'Part2: {part2()}')
 
 
 def part1(boxes):
-    for box in boxes:
-        lowest_distance = 9999999999999999999999999999999999999999999.0
-        for box2 in boxes:
-            distance = get_distance(box, box2)
-            if distance == 0:
-                continue
-            elif distance < lowest_distance:
-                lowest_distance = distance
-        print(lowest_distance)  # todo
+    networks = []
+    for i in range(10):
+        nearest_boxes = find_lowest_distance_boxes(boxes, networks)
+        box1 = nearest_boxes[0]
+        box2 = nearest_boxes[1]
+        is_box1_in_network = is_in_any_network(box1, networks)
+        is_box2_in_network = is_in_any_network(box2, networks)
+        # check if they're already in any network
+        if is_box1_in_network or is_box2_in_network:
+            # if both are in a different network, move all boxes from second network to first and remove second network
+            if is_box1_in_network and is_box2_in_network:
+                box1_network_index = get_box_network(box1, networks)
+                box2_network_index = get_box_network(box2, networks)
+                combined_network = combine_networks(networks[box1_network_index], networks[box2_network_index])
+                networks[box1_network_index] = combined_network
+                networks.pop(box2_network_index)
+            # if only one is in a network, add them to the existing network
+            elif is_box1_in_network:
+                box_network_index = get_box_network(box1, networks)
+                networks[box_network_index].boxes.append(box2)
+            elif is_box2_in_network:
+                box_network_index = get_box_network(box2, networks)
+                networks[box_network_index].boxes.append(box1)
+        # if no, create new network
+        else:
+            networks.append(Network(box1, box2))
+        i += 1
+
+    for network in networks:
+        print(len(network.boxes))  # todo
     return 0
 
 
@@ -50,6 +79,47 @@ def part2():
 def get_distance(box1, box2):
     """https://en.wikipedia.org/wiki/Euclidean_distance"""
     return math.sqrt((box1.x - box2.x)**2 + (box1.y - box2.y)**2 + (box1.z - box2.z)**2)
+
+
+def find_lowest_distance_boxes(boxes, networks):
+    lowest_distance = 9999999999999999999999999999999999999999999.0
+    nearest_boxes = []
+    for box1 in boxes:
+        for box2 in boxes:
+            distance = get_distance(box1, box2)
+            if distance == 0 or is_in_same_network(box1, box2, networks):
+                continue
+            elif distance < lowest_distance:
+                lowest_distance = distance
+                nearest_boxes = [box1, box2]
+
+    return nearest_boxes
+
+
+def is_in_same_network(box1, box2, networks):
+    for network in networks:
+        if box1 in network.boxes and box2 in network.boxes:
+            return True
+    return False
+
+
+def is_in_any_network(box, networks):
+    for network in networks:
+        if box in network.boxes:
+            return True
+    return False
+
+
+def get_box_network(box, networks):
+    for i in range(len(networks)):
+        if box in networks[i].boxes:
+            return i
+
+
+def combine_networks(network1, network2):
+    for box in network2.boxes:
+        network1.boxes.append(box)
+    return network1
 
 
 if __name__ == '__main__':

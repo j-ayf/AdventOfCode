b0d8031c274756044e2566c49056094a70dc5f44
Janik Frost <janik.frost@tui.com>
2025-12-09 16:46:12 +0100

    2025 Day 9 Part 2 - Add function to check if a point is in a rectangle

--- 2025/09/main.py
+++ 2025/09/main.py
@@ -13,7 +13,7 @@ def main():
     puzzle_input = get_input()
 
     print(f'Part1: {part1(puzzle_input)}')
-    print(f'Part2: {part2()}')
+    print(f'Part2: {part2(puzzle_input)}')
 
 
 def part1(coords):
@@ -27,8 +27,19 @@ def part1(coords):
     return largest_area
 
 
-def part2():
-    return 0
+def part2(coords):
+    largest_area = 0
+    for corner1 in coords:
+        for corner2 in coords:
+            if corner1 == corner2:
+                continue
+            is_valid = False
+            # add validation
+            if is_valid:
+                area = calc_area(corner1, corner2)
+                if area > largest_area:
+                    largest_area = area
+    return largest_area
 
 
 def calc_area(corner1, corner2):
@@ -37,6 +48,14 @@ def calc_area(corner1, corner2):
     side1 = max(x_coords) - min(x_coords) + 1
     side2 = max(y_coords) - min(y_coords) + 1
     return side1 * side2
+
+
+def is_point_in_rectangle(corner1, corner2, point):
+    if point == corner1 or point == corner2:
+        return False
+    x_coords = [min([corner1[0], corner2[0]]), max([corner1[0], corner2[0]])]
+    y_coords = [min([corner1[1], corner2[1]]), max([corner1[1], corner2[1]])]
+    return x_coords[0] < point[0] < x_coords[1] and y_coords[0] < point[1] < y_coords[1]
 
 
 if __name__ == '__main__':

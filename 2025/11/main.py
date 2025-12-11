d7f11c044a3257c8cdc89b0892f6d57b910d2e25
Janik Frost <janik.frost@tui.com>
2025-12-11 16:35:25 +0100

    2025 Day 11 Part 1 - With recursion and global variable

--- 2025/11/main.py
+++ 2025/11/main.py
@@ -2,23 +2,50 @@
 
 def get_input():
     with open('puzzleInput.txt', 'r') as f:
-        return f.read().rstrip().split('\n')
+        lines = f.read().rstrip().split('\n')
+        devices = []
+        for line in lines:
+            temp = line.split(': ')
+            device = [temp[0], temp[1].split()]
+            devices.append(device)
+    return devices
 
 
 def main():
     puzzle_input = get_input()
 
-    print(f'Part1: {part1()}')
+    print(f'Part1: {part1(puzzle_input)}')
     print(f'Part2: {part2()}')
 
 
-def part1():
-    return 0
+def part1(devices):
+    start_i = get_device_index(devices, 'you')
+    find_paths_to_out(devices, start_i)
+    global paths
+    return paths
 
 
 def part2():
     return 0
 
 
+def get_device_index(devices, device_name):
+    for device in devices:
+        if device[0] == device_name:
+            return devices.index(device)
+
+
+def find_paths_to_out(devices, current_index):
+    device = devices[current_index]
+    for output in device[1]:
+        if output == 'out':
+            global paths
+            paths += 1
+            return
+        new_index = get_device_index(devices, output)
+        find_paths_to_out(devices, new_index)
+
+
 if __name__ == '__main__':
+    paths = 0
     main()

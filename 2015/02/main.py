00808ce5a6e7047ea0d9d70eb17ee1357bed93e8
J-ayf <100358455+j-ayf@users.noreply.github.com>
2025-12-15 16:19:34 +0100

    2015 Day 2 Part 1

--- 2015/02/main.py
+++ 2015/02/main.py
@@ -6,17 +6,35 @@ def get_input():
 
 def main():
     puzzle = get_input()
-    print(puzzle)
 
     print(f'Part1: {part1(puzzle)}')
     print(f'Part2: {part2(puzzle)}')
 
 def part1(puzzle):
-    return 0
+    total = 0
+    for present in puzzle:
+        l, w, h = get_dimensions(present)
+        total += get_total_area(l, w, h)
+        total += get_smallest_area(l, w, h)
+    return total
 
 
 def part2(puzzle):
     return 0
+
+
+def get_dimensions(dimensions):
+    dims = dimensions.split('x')
+    return int(dims[0]), int(dims[1]), int(dims[2])
+
+
+def get_total_area(l, w, h):
+    return 2*l*w + 2*w*h + 2*h*l
+
+
+def get_smallest_area(l, w, h):
+    areas = [l*w, w*h, h*l]
+    return min(areas)
 
 
 if __name__ == '__main__':

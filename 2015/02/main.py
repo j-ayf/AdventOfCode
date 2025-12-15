38a21d55aa87d62808fdcb6acad318d6bd30a7d7
J-ayf <100358455+j-ayf@users.noreply.github.com>
2025-12-15 16:36:58 +0100

    2015 Day 2 Part 2

--- 2015/02/main.py
+++ 2015/02/main.py
@@ -20,7 +20,12 @@ def part1(puzzle):
 
 
 def part2(puzzle):
-    return 0
+    total = 0
+    for present in puzzle:
+        l, w, h = get_dimensions(present)
+        total += get_base_ribbon_length(l, w, h)
+        total += l*w*h
+    return total
 
 
 def get_dimensions(dimensions):
@@ -35,6 +40,19 @@ def get_total_area(l, w, h):
 def get_smallest_area(l, w, h):
     areas = [l*w, w*h, h*l]
     return min(areas)
+
+
+def get_base_ribbon_length(l, w, h):
+    dims = [l, w, h]
+    longest_i = dims.index(max(dims))
+    if longest_i == 0:
+        small_i = [1, 2]
+    elif longest_i == 1:
+        small_i = [0, 2]
+    else:
+        small_i = [0, 1]
+
+    return 2*dims[small_i[0]] + 2*dims[small_i[1]]
 
 
 if __name__ == '__main__':

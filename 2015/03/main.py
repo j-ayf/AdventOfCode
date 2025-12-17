0bcef369b7273ca3247aa64a1fc44a76eed500dd
J-ayf <100358455+j-ayf@users.noreply.github.com>
2025-12-17 13:07:24 +0100

    2015 Day 3 Part 1

--- 2015/03/main.py
+++ 2015/03/main.py
@@ -2,21 +2,43 @@
 
 def get_input():
     with open('input.txt', 'r') as f:
-        return f.read().rstrip().split('\n')
+        return f.read().rstrip()
 
 def main():
     puzzle = get_input()
-    print(puzzle)
 
     print(f'Part1: {part1(puzzle)}')
     print(f'Part2: {part2(puzzle)}')
 
 def part1(puzzle):
-    return 0
+    current_coords = [0,0]
+    delivered_presents = {str(current_coords): 1}
+    for direction in puzzle:
+        current_coords =  get_new_coords(current_coords, direction)
+        if str(current_coords) in delivered_presents:
+            delivered_presents[str(current_coords)] += 1
+        else:
+            delivered_presents[str(current_coords)] = 1
+
+    return len(delivered_presents)
 
 
 def part2(puzzle):
     return 0
+
+
+def get_new_coords(current_coords, direction):
+    if direction == '^':
+        return [current_coords[0], current_coords[1]+1]
+    elif direction == '>':
+        return [current_coords[0]+1, current_coords[1]]
+    elif direction == 'v':
+        return [current_coords[0], current_coords[1]-1]
+    elif direction == '<':
+        return [current_coords[0]-1, current_coords[1]]
+    else:
+        raise Exception(f'Invalid direction "{direction}"')
+
 
 
 if __name__ == '__main__':

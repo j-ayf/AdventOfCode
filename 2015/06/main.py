5e40fa45f02e57aacdbc0b80db27560d123dc09c
J-ayf <100358455+j-ayf@users.noreply.github.com>
2025-12-17 16:35:54 +0100

    2015 Day 6 Part 1

--- 2015/06/main.py
+++ 2015/06/main.py
@@ -1,18 +1,89 @@
-# https://adventofcode.com/2015/day/2
+# https://adventofcode.com/2015/day/6
+
+
+class Light:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.is_on = False
+
+    def on(self):
+        self.is_on = True
+
+    def off(self):
+        self.is_on = False
+
+    def toggle(self):
+        if self.is_on:
+            self.is_on = False
+        else:
+            self.is_on = True
+
+    def __repr__(self):
+        return f'{self.x}, {self.y}, {self.is_on}'
+
+
+def make_lights():
+    lights = []
+    for i in range(1000):
+        row = []
+        for j in range(1000):
+            row.append(Light(j, i))
+        lights.append(row)
+    return lights
+
 
 def get_input():
     with open('input.txt', 'r') as f:
-        return f.read().rstrip().split('\n')
+        lines = f.read().rstrip().split('\n')
+    instructions = []
+    for line in lines:
+        l = line.split(' through ')
+        end = get_int_coords(l[-1])
+        l = l[0].split()
+        start = get_int_coords(l[-1])
+        if len(l) == 3:
+            instruction = l[1]
+        else:
+            instruction = l[0]
+
+        instructions.append([instruction, start, end])
+    return instructions
+
+
+def get_int_coords(str_coords):
+    str_coords = str_coords.split(',')
+    return [int(str_coords[0]), int(str_coords[1])]
+
 
 def main():
     puzzle = get_input()
-    print(puzzle)
+    lights = make_lights()
 
-    print(f'Part1: {part1(puzzle)}')
+    print(f'Part1: {part1(puzzle, lights)}')
     print(f'Part2: {part2(puzzle)}')
 
-def part1(puzzle):
-    return 0
+
+def part1(puzzle, lights):
+    for instruction in puzzle:
+        start = instruction[1]
+        end = instruction[2]
+        for i in range(start[0], end[0]+1):
+            for j in range(start[1], end[1]+1):
+                light = lights[i][j]
+                if instruction[0] == 'on':
+                    light.on()
+                if instruction[0] == 'off':
+                    light.off()
+                if instruction[0] == 'toggle':
+                    light.toggle()
+
+    total = 0
+    for row in lights:
+        for light in row:
+            if light.is_on:
+                total += 1
+    return total
 
 
 def part2(puzzle):
